1cd4c54a62c34bdfc4273e5d57ce253896ce27d9
airslice <icesunex@hotmail.com>
2024-08-22 10:41:25 +0800

    refactor: update types

--- src/shared/reearthTypes/index.ts
+++ src/shared/reearthTypes/index.ts
@@ -1,34 +1,14 @@
-import { Camera } from "./camera";
-import { Data } from "./data";
-import { Engine } from "./engine";
-import { Extension } from "./extension";
-import { Layers } from "./layers";
-import { Modal } from "./modal";
-import { Popup } from "./popup";
-import { Sketch } from "./sketch";
-import { Timeline } from "./timeline";
-import { UI } from "./ui";
-import { Viewer } from "./viewer";
-
-export declare type Reearth = {
-  // system
-  readonly version: string;
-  readonly apiVersion: string;
-  readonly engine: Engine;
-  // viewer
-  readonly viewer: Viewer;
-  readonly camera: Camera;
-  readonly timeline: Timeline;
-  // functions
-  readonly sketch: Sketch;
-  // layers
-  readonly layers: Layers;
-  // ui
-  readonly ui: UI;
-  readonly modal: Modal;
-  readonly popup: Popup;
-  // extension
-  readonly extension: Extension;
-  // data
-  readonly data: Data;
-};
+export * from "./globe";
+export * from "./reearth";
+export * from "./engine";
+export * from "./viewer";
+export * from "./camera";
+export * from "./data";
+export * from "./timeline";
+export * from "./sketch";
+export * from "./layers";
+export * from "./ui";
+export * from "./modal";
+export * from "./popup";
+export * from "./extension";
+export * from "./data";
